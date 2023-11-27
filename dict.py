35119cf360b8c716890dd33c7f4faebf017ef31e
ajay2java <142924076+ajay2java@users.noreply.github.com>
2023-11-26 22:28:56 -0500

    save

---
+++ dict.py
@@ -0,0 +1,45 @@
+# FME = []
+
+general = {
+    'One Credit': ['IMH25', 'IMH26'],
+    'Two Credit': [],
+    'FME': ['FME1000', 'FME1001', 'EPS1000', 'MOB1010'], 
+    'Standard': ['ACC1000', 'LAW1000', 'AQM1000', 'AQM2000'],
+    'Adv_Experiential': [],
+    'Adv_LiberalArts': ['If >= 4600'],
+    'Adv_LiberalArts_Elec': ['If >= 3600']
+}
+# ex: if CSP 20 (from dict) is in CSP2056 (from person's excel)
+
+accounting = {
+    'Required': [],
+    'Elective': []
+}
+
+business_analytics = {
+    "Data Management and Programming Concepts": [],
+    'Advanced Data and Decision Modeling': [],
+    'Electives': []
+}
+
+comp_math_fin = {
+    'Required': [],
+    'Elective_1': [],
+    'Elective_2': []
+}
+
+econ = {
+    'Required': [],
+    'Elective': []
+}
+
+entre = {
+    
+}
+
+finance = {
+    
+}
+
+#babson study abroad is 17 credits!!!
+
