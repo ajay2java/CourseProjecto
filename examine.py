4314e6662ac2a8876df827e05d4e05052aab8a4d
ajay2java <142924076+ajay2java@users.noreply.github.com>
2023-11-29 01:47:04 -0500

    save

---
+++ examine.py
@@ -0,0 +1,63 @@
+import csv
+
+workbook = data(filename="")
+
+#Fall Year 1: A2
+#Spring Year 1: A11
+
+#Fall Year 2: E2
+#Spring Year 2: E11
+
+#Fall Year 3: I2
+#Spring Year 3: I11
+
+#Fall Year 4: M2
+#Spring Year 4: M11
+
+###   Analytics Concentration  ###
+# def course_by_course():
+    
+    #while ___[each course input from the user's spreadsheet]__(recursive)
+        ### will cycle through the excel for each course ###
+        #if conc == "Business Analytics"
+            #if header == "Data Management and Programming Concepts":
+                #if Ana_DataMgmt_Prog == 1:
+                    #Ana_Elec += 1
+                #else:
+                    #Ana_DataMgmt_Prog += 1:
+            #elif header == "Advanced Data and Decision Modeling":
+                #if Ana_Data_DecM == 1:
+                    #Ana_Elec += 1
+                #else:
+                    #Ana_Data_DecM += 1:
+        #if conc == "Finance":
+
+    #if Ana_DataMgmt_Prog == 1 and Ana_Data_DecM == 1 and Ana_Elec == 2:
+    ##print("You have fulfilled all the requirements for the Analytics Concentration!")
+
+
+class Concentration:
+    
+    def __innit__(name, excel):
+        ###
+        name.excel = excel
+    
+    def __str__(name):
+        name.analytics(name.excel)
+    
+    def analytics(name, excel):
+        for obj in excel:
+            #if (my course name) is in analytics course list,
+            # +=1
+        
+        if ___=___ and ___>=___ and ___ >= ___:
+            return True
+
+
+### code that takes all courses from excel and puts them into a list##
+course_list = []
+#for i in range (len(excel)):
+    #course_list.append(i)
+
+Ajay = Concentration("Ajay", course_list)
+print(Ajay)
