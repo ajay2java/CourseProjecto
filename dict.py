2b0f50e47604bd402110850f2523773644f81bd3
ajay2java <142924076+ajay2java@users.noreply.github.com>
2023-11-30 13:44:21 -0500

    save

--- dict.py
+++ dict.py
@@ -3,11 +3,13 @@
 general = {
     'One Credit': ['IMH25', 'IMH26'],
     'Two Credit': [],
+    'Three Credit': [],
+    'Six Credit': [],
     'FME': ['FME1000', 'FME1001', 'EPS1000', 'MOB1010'], 
     'Standard': ['ACC1000', 'LAW1000', 'AQM1000', 'AQM2000'],
-    'Adv_Experiential': [],
-    'Adv_LiberalArts': ['If >= 4600'],
-    'Adv_LiberalArts_Elec': ['If >= 3600']
+    'Adv_Experiential': ['COM3504', 'COM3522', 'ECN3600', 'ECN3601', 'EPS3534', 'EPS4515', 'FIN3560', 'MKT3540', 'MKT3575', 'MKT4505', 'MKT4520', 'MOB3534', 'OIM3536', 'SCN3602', 'STR 3508', 'BCF'],
+    #'Adv_LiberalArts': ['If >= 4600'],
+    #'Adv_LiberalArts_Elec': ['If >= 3600']
 }
 # ex: if CSP 20 (from dict) is in CSP2056 (from person's excel)
 
--- examine.py
+++ examine.py
@@ -1,6 +1,9 @@
 import csv
 
+from dict import general, business_analytics
 workbook = data(filename="")
+
+num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 #Fall Year 1: A2
 #Spring Year 1: A11
@@ -38,20 +41,142 @@ workbook = data(filename="")
 
 class Concentration:
     
-    def __innit__(name, excel):
+    def __innit__(self, name, total_creds, ana_dm, ana_ad, ana_e, course_list=None):
         ###
-        name.excel = excel
+        self.name = name
+        self.total_creds = 0
+        self.fMe = 0
+        self.adv_lib = 0
+        self.adv_libe = 0
+        self.ana_dm = ana_dm
+        self.ana_ad = ana_ad
+        self.ana_e = ana_e
+        if course_list is None:
+            self.courses = []
+        else:
+            self.courses = course_list
     
-    def __str__(name):
-        name.analytics(name.excel)
-    
-    def analytics(name, excel):
-        for obj in excel:
-            #if (my course name) is in analytics course list,
-            # +=1
+    def __str__(self):
+        t = [f"{self.name} College Course Status:"]
+        if self.total_creds >= 128:
+            s = "Total Credits Status: ✅"
+            t.append(s)
+        else:
+            s = "Total Credits Status: ❌"
+            t.append(s)
         
-        if ___=___ and ___>=___ and ___ >= ___:
-            return True
+        if self.fMe >= 2:
+            s = "FME/EPS Requirement: ✅"
+            t.append(s)
+        else:
+            s = "FME/EPS Requirements: ❌"
+            t.append(s)
+
+        if self.std >= 20:
+            s = "Standard Requirements: ✅"
+            t.append(s)
+        else:
+            s = "Standard Requirements: ❌"
+            t.append(s)
+
+        if self.adv_lib >= 4: # num in credits
+            s = "Advanced Liberal Arts: ✅"
+            t.append(s)
+        else:
+            s = "Advanced Liberal Arts: ❌"
+            t.append(s)
+        
+        if self.adv_libe >= 12:
+            s = "Advanced Liberal Arts Elective: ✅"
+            t.append(s)
+        else:
+            s = "Advanced Liberal Arts Elective: ❌"
+            t.append(s)
+        
+        if self.ana_dm >= 1 and self.ana_ad >= 1 and self.ana_e >= 2:
+            s = "Business Analytics Concentration: ✅"
+            t.append(s)
+        # repeat for other concentrations
+        
+        return "\n".join(t)
+            
+    
+    def num_credits(self):
+        for a in self.courses:
+            if a in general['One Credit']:
+                self.total_creds += 1
+            elif a in general['Two Credit']:
+                self.total_creds += 2
+            elif a in general['Three Credit']:
+                self.total_creds += 3
+            elif a in general['Six Credit']:
+                self.total_creds += 6
+            elif a in general['Seventeen Credit']:
+                self.total_creds += 17
+            else:
+                self.total_creds += 4
+    
+    
+    def get_num_creds(self, course):
+        if course in general['One Credit']:
+            return 1
+        elif course in general['Two Credit']:
+            return 2
+        elif course in general['Three Credit']:
+            return 3
+        elif course in general['Six Credit']:
+            return 6
+        elif course in general['Seventeen Credit']:
+            return 17
+        else:
+            return 4
+
+    def FME(self):
+        for a in self.courses:
+            if a in general['FME']:
+                self.fMe += 1
+
+    def standard(self):
+        for a in self.courses:
+            if a in general['Standard']:
+                self.std += 1
+
+    def adv_libarts(self):
+        store = ""
+        for a in self.courses:
+            for i in range(len(a)):
+                if a[i] in num:
+                    store += a[i]
+            if int(store) >= 4600:
+                self.adv_lib += self.get_num_creds(a)
+            store = ""
+
+    def adv_libarts_elec(self):
+        store = ""
+        for a in self.courses:
+            for i in range(len(a)):
+                if a[i] in num:
+                    store += a[i]
+            if int(store) >= 3600:
+                self.adv_libe += self.get_num_creds(a)
+            store = ""
+
+    def analytics(self):
+        for a in self.courses:
+            if a in business_analytics['Data Management and Programming Concepts']:
+                if self.ana_dm == 1:
+                    self.ana_e += self.get_num_creds(a)
+                else:
+                    self.ana_dm += 1
+            elif a in business_analytics['Advanced Data and Decision Modeling']:
+                if self.ana_ad == 1:
+                    self.ana_e += self.get_num_creds(a)
+                else:
+                    self.ana_ad += 1
+            elif a in business_analytics['Electives']:
+                self.ana_e += self.get_num_creds(a)
+            else:
+                continue
 
 
 ### code that takes all courses from excel and puts them into a list##
@@ -59,5 +184,8 @@ course_list = []
 #for i in range (len(excel)):
     #course_list.append(i)
 
-Ajay = Concentration("Ajay", course_list)
+pre_req = input("How many credits from pre-Babson do you have, which counts?")
+
+Ajay = Concentration("Ajay", course_list, pre_req)
 print(Ajay)
+
