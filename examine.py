605bab90384efee3530502f4f289ebe93eceafe0
ajay2java <142924076+ajay2java@users.noreply.github.com>
2023-12-01 15:32:43 -0500

    save

--- examine.py
+++ examine.py
@@ -1,9 +1,7 @@
-import csv
+# import csv
 
 from dict import general, business_analytics
 
-# with open("data/Course_template.csv", "r") as file:
-#     reader = csv.reader(file)
 
 from openpyxl import load_workbook
 workbook = load_workbook(filename="data/Course_template.xlsx")
@@ -13,39 +11,6 @@ workbook.sheetnames
 sheet = workbook.active
 
 num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-#Fall Year 1: A2
-#Spring Year 1: A11
-
-#Fall Year 2: E2
-#Spring Year 2: E11
-
-#Fall Year 3: I2
-#Spring Year 3: I11
-
-#Fall Year 4: M2
-#Spring Year 4: M11
-
-###   Analytics Concentration  ###
-# def course_by_course():
-    
-    #while ___[each course input from the user's spreadsheet]__(recursive)
-        ### will cycle through the excel for each course ###
-        #if conc == "Business Analytics"
-            #if header == "Data Management and Programming Concepts":
-                #if Ana_DataMgmt_Prog == 1:
-                    #Ana_Elec += 1
-                #else:
-                    #Ana_DataMgmt_Prog += 1:
-            #elif header == "Advanced Data and Decision Modeling":
-                #if Ana_Data_DecM == 1:
-                    #Ana_Elec += 1
-                #else:
-                    #Ana_Data_DecM += 1:
-        #if conc == "Finance":
-
-    #if Ana_DataMgmt_Prog == 1 and Ana_Data_DecM == 1 and Ana_Elec == 2:
-    ##print("You have fulfilled all the requirements for the Analytics Concentration!")
 
 
 class Concentration:
@@ -66,7 +31,6 @@ class Concentration:
             self.courses = []
         else:
             self.courses = course_list
-        print(self.courses)
     
     def __str__(self):
         t = [f"{self.name} College Course Status:"]
@@ -205,40 +169,30 @@ class Concentration:
 
 
 ### code that takes all courses from excel and puts them into a list##
-# course_list = [sheet["A2"].value, sheet["A3"].value, sheet["A4"].value, sheet["A5"].value, sheet["A6"].value,
-#                sheet["A11"].value, sheet["A12"].value, sheet["A13"].value, sheet["A14"].value, sheet["A15"].value,
+course_list = [sheet["A2"].value, sheet["A3"].value, sheet["A4"].value, sheet["A5"].value, sheet["A6"].value,
+               sheet["A11"].value, sheet["A12"].value, sheet["A13"].value, sheet["A14"].value, sheet["A15"].value,
 
-#                sheet["E2"].value, sheet["E3"].value, sheet["E4"].value, sheet["E5"].value, sheet["E6"].value,
-#                sheet["E11"].value, sheet["E12"].value, sheet["E13"].value, sheet["E14"].value, sheet["E15"].value,
+               sheet["E2"].value, sheet["E3"].value, sheet["E4"].value, sheet["E5"].value, sheet["E6"].value,
+               sheet["E11"].value, sheet["E12"].value, sheet["E13"].value, sheet["E14"].value, sheet["E15"].value,
 
-#                sheet["I2"].value, sheet["I3"].value, sheet["I4"].value, sheet["I5"].value, sheet["I6"].value,
-#                sheet["I11"].value, sheet["I12"].value, sheet["I13"].value, sheet["I14"].value, sheet["I15"].value,
+               sheet["I2"].value, sheet["I3"].value, sheet["I4"].value, sheet["I5"].value, sheet["I6"].value,
+               sheet["I11"].value, sheet["I12"].value, sheet["I13"].value, sheet["I14"].value, sheet["I15"].value,
                
-#                sheet["M2"].value, sheet["M3"].value, sheet["M4"].value, sheet["M5"].value, sheet["M6"].value,
-#                sheet["M11"].value, sheet["M12"].value, sheet["M13"].value, sheet["M14"].value, sheet["M15"].value]
-
-course_list = [sheet["A2"].value, sheet["A3"].value, sheet["A4"].value, sheet["A5"].value,
-               sheet["A11"].value, sheet["A12"].value, sheet["A13"].value, sheet["A14"].value,
-
-               sheet["E2"].value, sheet["E3"].value, sheet["E4"].value, sheet["E5"].value,
-               sheet["E11"].value, sheet["E12"].value, sheet["E13"].value, sheet["E14"].value,
-
-               sheet["I2"].value, sheet["I3"].value, sheet["I4"].value, sheet["I5"].value,
-               sheet["I11"].value, sheet["I12"].value, sheet["I13"].value, sheet["I14"].value,
-               
-               sheet["M2"].value, sheet["M3"].value, sheet["M4"].value, sheet["M5"].value,
-               sheet["M11"].value, sheet["M12"].value, sheet["M13"].value, sheet["M14"].value]
-
-print(len(course_list))
-# for z in range(len(course_list)):
-#     if course_list[z] == None:
-#         course_list.pop(z)
-#         z = z - 1
+               sheet["M2"].value, sheet["M3"].value, sheet["M4"].value, sheet["M5"].value, sheet["M6"].value,
+               sheet["M11"].value, sheet["M12"].value, sheet["M13"].value, sheet["M14"].value, sheet["M15"].value]
 
 
+
+
+for z in course_list:
+    if z == sheet["B3"].value:
+        course_list.remove(z)
+
+
+    
 
 #Put an int check on this!
-pre_req = input("How many credits from pre-Babson do you have, which counts?")
+pre_req = input("How many credits from pre-Babson do you have, which counts? ")
 
 Ajay = Concentration("Ajay", pre_req, course_list)
 Ajay.num_credits()
@@ -249,5 +203,3 @@ Ajay.adv_libarts_elec()
 Ajay.analytics()
 
 print(Ajay)
-
-# print(sheet["A2"].value)
